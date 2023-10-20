2fe61a5713f1e94a8ee5d528184f1e205b51586c
Bimbolo <afsilva6236@misena.edu.co>
2023-10-20 08:57:31 -0500

    Commit inicial

---
+++ app/src/main/java/csadsalazar/my_application_cesar/MainActivity.kt
@@ -0,0 +1,46 @@
+package csadsalazar.my_application_cesar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import csadsalazar.my_application_cesar.ui.theme.My_Application_CesarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            My_Application_CesarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Kotlin!!!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    My_Application_CesarTheme {
+        Greeting("Kotlin!!!")
+    }
+}
