e461b04c3de9adf3a0ebfdfd3e86ac4439d666ce
Bimbolo <afsilva6236@misena.edu.co>
2023-10-20 10:20:57 -0500

    Se crea data class message y MessageCard

--- app/src/main/java/csadsalazar/my_application_cesar/MainActivity.kt
+++ app/src/main/java/csadsalazar/my_application_cesar/MainActivity.kt
@@ -3,44 +3,34 @@ package csadsalazar.my_application_cesar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import csadsalazar.my_application_cesar.ui.theme.My_Application_CesarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            My_Application_CesarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Kotlin!!!")
-                }
-            }
-        }
-    }
-}
+            MessageCard(Message("Android", "Jetpack Compose"))
+        } //setContent
+    } //onCreate
+} //MainActivity
+
+data class Message(val author: String, val body: String)
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    My_Application_CesarTheme {
-        Greeting("Kotlin!!!")
-    }
+fun MessageCardPreview() {
+    MessageCard(
+    msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+)
 }
